78685329ad60c17f0e3f2a8ee929eca37aa95be7
chriscebrero <chris.cebrero@gmail.com>
2018-06-24 14:38:45 -0700

    added main component

--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Switch, Route, withRouter, Redirect} from "react-router-dom";
+import {connect} from "react-redux";
+import Homepage from "../components/Homepage";
 
 const Main = props => {
 	return(
@@ -7,5 +9,14 @@ const Main = props => {
 			<Switch>
 				<Route exact path="/" render={props => <Homepage {...props} />} />
 			</Switch>
-		</div>)
+		</div>
+		);
+};
+
+function mapStateToProps(state) {
+	return{
+		currentUser: state.currentUser
+	}
 }
+
+export default withRouter(connect(mapStateToProps)(Main));
