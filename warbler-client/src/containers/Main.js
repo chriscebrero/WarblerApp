0904b81c9f76df319badb3b7ee8d1a9a5dd9fd78
chriscebrero <chris.cebrero@gmail.com>
2018-06-24 14:34:05 -0700

    added main component

---
+++ warbler-client/src/containers/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Switch, Route, withRouter, Redirect} from "react-router-dom";
+
+const Main = props => {
+	return(
+		<div className="container">
+			<Switch>
+				<Route exact path="/" render={props => <Homepage {...props} />} />
+			</Switch>
+		</div>)
+}
