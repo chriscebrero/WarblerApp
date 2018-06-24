803f662a21273477a9405f9bfb0b90bec2006c31
chriscebrero <chris.cebrero@gmail.com>
2018-06-23 20:39:45 -0700

    added currentuser export

--- warbler-client/src/store/reducers/index.js
+++ warbler-client/src/store/reducers/index.js
@@ -1,1 +1,10 @@
 import {combinedReducers} from "redux";
+import currentUser from "./currentUser";
+import errors from "./errors";
+
+const rootReducer = combinedReducers({
+	currentUser,
+	errors
+});
+
+export default rootReducer;
