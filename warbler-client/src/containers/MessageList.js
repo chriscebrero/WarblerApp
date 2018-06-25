b94df59d3f99f64ad64d7b16854ae9549b4b8bd9
chriscebrero <chris.cebrero@gmail.com>
2018-06-24 20:21:11 -0700

    create mapstatetoprops in messagelist

--- warbler-client/src/containers/MessageList.js
+++ warbler-client/src/containers/MessageList.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {fetchMessages} from "../store/actions/messages";
+import MessageItem from "../components/MessageItem";
 
 class MessageList extends Component {
+	componentDidMount() {
+		this.props.fetchMessages();
+	}
+	render() {
+		const {messages} = this.props
+		let messageList = messages.map(m => (
+			<MessageItem 
+			key={m._id} 
+			date={m.createAt} 
+			text={m.text} 
+			username{m.user.username} 
+			profileImageUrl={m.user.profileImageUrl}
+			/>
+			));
+	}
 
 }
 
