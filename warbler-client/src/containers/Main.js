b47fd7bfb17fa83ec9883912a7f53e65aa130919
chriscebrero <chris.cebrero@gmail.com>
2018-06-24 18:23:20 -0700

    fixed

--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -5,13 +5,19 @@ import Homepage from "../components/Homepage";
 import AuthForm from "../components/AuthForm";
 import { authUser } from "../store/actions/auth";
 import { removeError } from "../store/actions/errors";
+import withAuth from "../hocs/withAuth";
+import MessageForm from "../containers/MessageForm";
 
 const Main = props => {
-  const { authUser, errors, removeError } = props;
+  const { authUser, errors, removeError, currentUser } = props;
   return (
     <div className="container">
       <Switch>
-        <Route exact path="/" render={props => <Homepage {...props} />} />
+        <Route
+          exact
+          path="/"
+          render={props => <Homepage currentUser={currentUser} {...props} />}
+        />
         <Route
           exact
           path="/signin"
@@ -52,6 +58,7 @@ const Main = props => {
 
 function mapStateToProps(state) {
   return {
+    currentUser: state.currentUser,
     errors: state.errors
   };
 }
--- warbler-server/models/index.js
+++ warbler-server/models/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/warbler", {
-	keepAlive: true
+  keepAlive: true
 });
 
 module.exports.User = require("./user");
