3176bcd9baa15320facf0069358457f51a855382
chriscebrero <chris.cebrero@gmail.com>
2018-06-24 18:31:57 -0700

    added log in and log out

--- warbler-client/src/containers/Navbar.js
+++ warbler-client/src/containers/Navbar.js
@@ -13,6 +13,17 @@ class Navbar extends Component {
               <img src={Logo} alt="Warbler Home" />
             </Link>
           </div>
+          {this.props.currentUser.isAuthenticated ? (
+            <ul className="nav-navbar-nav navbar-right">
+            <li>
+              <Link to={`/users/${this.props.currentUser.user.id/messages/new}`}>New Message</Link>
+            </li>
+            <li>
+              <a onClick={this.logout}>Log out</a>
+            </li>
+            </ul>
+            )
+          }
           <ul className="nav navbar-nav navbar-right">
             <li>
               <Link to="/signup">Sign up</Link>
