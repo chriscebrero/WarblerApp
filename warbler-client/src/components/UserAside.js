b9575fa62fe38436a0ef55e0024dc43baae5676e
chriscebrero <chris.cebrero@gmail.com>
2018-06-24 22:29:40 -0700

    added useraside

--- warbler-client/src/components/UserAside.js
+++ warbler-client/src/components/UserAside.js
@@ -5,10 +5,11 @@ const UserAside = ({profileImageUrl, username}) => (
 	<aside className="col-sm-2">
 		<div className="panel panel-default">
 			<div className="panel-body">
-				<img src={profileImageUrl || DefaultProfileImg} alt={username} className="img-thumbnail"
+				<img src={profileImageUrl || DefaultProfileImg} alt={username} width="200" height="200" className="img-thumbnail"
 				/>
 			</div>
 		</div>
 	</aside>
 );
 
+export default UserAside;
